e5c21a91d00a69bf39dfd8803786e34ceb353e98
voocel <voocel@gmail.com>
2017-07-14 23:51:30 +0800

    slideshow

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -5,6 +5,9 @@ class Index extends Base
 {
     public function index()
     {
-        return $this->fetch();
+        $slideshows = model('featured')->getFeaturedsByType(0);
+        $slidepic = model("featured")->where(['status'=>['neq',-1],'type'=>1])->order(["id"=>'desc'])->find();
+        
+        return $this->fetch('',['slideshows'=>$slideshows,"slidepic"=>$slidepic]);
     }
 }
